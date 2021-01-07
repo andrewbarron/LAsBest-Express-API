f23b70aca736bea04df39810162ac5b758f1e6da
andrewbarron <drewjbarron@gmail.com>
2021-01-07 12:27:05 -0800

    will this work
    .

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -15,10 +15,13 @@ const restaurantSchema = new mongoose.Schema({
   price: {
     type: String,
     required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   }
 }, {
   timestamps: true
 })
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema)
-module.exports = Restaurant
+module.exports = mongoose.model('Restaurant', restaurantSchema)
--- app/routes/restaurant_routes.js
+++ app/routes/restaurant_routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
-const Restaurant = require('./models/restaurant')
+const Restaurant = require('../models/restaurant')
 const requireToken = passport.authenticate('bearer', { session: false })
 
 // create a restaurant
@@ -9,11 +9,11 @@ router.post('/restaurants', requireToken, (req, res, next) => {
   // get data from request
   const restaurantData = req.body.restaurant
   // add user as restaurant owner
-  restaurantData.owner = req.user._id
+  restaurantData.owner = req.params.id
   // create the restaurant
   Restaurant.create(restaurantData)
-    .then(restaurantData => {
-      res.status(201).json({ restaurantData: restaurantData })
-        .catch(next)
-    })
+    .then(restaurantData => res.status(201).json({ restaurantData: restaurantData }))
+    .catch(next)
 })
+
+module.exports = router
