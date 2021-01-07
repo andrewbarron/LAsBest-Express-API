8a86a28157562fa533a4b9c461585ff49d53cb17
andrewbarron <drewjbarron@gmail.com>
2021-01-07 13:00:16 -0800

    curl script
    index & show

---
+++ curl-scripts/restaurants/index.sh
@@ -0,0 +1,9 @@
+API="https://afternoon-wave-03144.herokuapp.com"
+URL_PATH="/restaurants"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request GET \
+  --header "Authorization: Bearer ${TOKEN}"
+
+echo
